8bbf7a08c831766f9aa556d00720bf67042be595
Juan Andres Velez <158998667+NicLockeFilms@users.noreply.github.com>
2025-01-15 19:30:17 +0100

    LogicaC#

---
+++ Silas/Models/Usuarios/UsuarioService.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace Silas.Models.Usuarios
+{
+    public class UsuarioService
+    {
+        private readonly HttpClient _httpClient;
+
+        public UsuarioService(HttpClient httpClient) { 
+         _httpClient = httpClient;
+        }
+
+        public async Task<bool> CrearUsuarioAsync (Usuario usuario) 
+        {
+        var json = JsonSerializer.Serialize(usuario);
+        var content = new StringContent (json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await _httpClient.PostAsync("", content);
+                response.EnsureSuccessStatusCode();
+                return response.IsSuccessStatusCode;
+            }
+            catch (HttpRequestException ex) { 
+            Console.WriteLine ($"Error en la solicitud: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
--- Silas/Program.cs
+++ Silas/Program.cs
@@ -1,6 +1,9 @@
+using Silas.Models.Usuarios;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient<UsuarioService>();	
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -22,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=Usuario}/{action=CrearUsuario}");
 
 app.Run();
